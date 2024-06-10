32c3b96a024cbb66532ca213d6d3c678acc27fa2
bernard powers <adarshsrivastava0348@gmail.com>
2024-06-10 17:32:33 +0530

    done

---
+++ backend/models/Data.js
@@ -0,0 +1,12 @@
+const DataSchema = require('mongoose');
+
+const DataSchema = new mongoose.Schema({
+    id:{ type: Number, required: true, unique: true},
+    name: String,
+    score: Number,
+    age: Number,
+    city: String,
+    gender: String,
+});
+
+module.exports = mongoose.model('Data', DataSchema);
